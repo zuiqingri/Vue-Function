2da1e45d35450bd2f46a542bac9c85e2058f4537
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-10-04 02:24:41 +0800

    Methods&Event

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,40 @@
 <template>
-  
-  <!-- <h1>Hello {{animal}}</h1>
-  <h2>{{food}}</h2>
-  <input type="text" v-model="food"/>
-  <div v-html="alertMessage"></div>
-
-  <a :href="food">{{food}}</a>
-  <hr/>
-  <h2 v-bind:class="{red:food==='apple','not-good':food==='rice'}"> I like {{food}}.</h2> -->
-  <h2>You age is {{age}}. </h2>
-  <h3 v-if="age>18">You are a adult.</h3>
-  <h3 v-else-if="age>13 && age<=18">You are a Youth.</h3>
-  <h3 v-else>You are a child</h3>
-   <input type="text" v-model="age"/>
-   <!-- <h2 v-if="display">Showed</h2>
-   <h2 v-show="display">Showed</h2>
-   <h2 v-for="(animal,index) in animals" :key="index">{{animal}}::{{index}}</h2>
-   <ul>
-     <li v-for="(user,index) in users" :key="index">{{user.name}}
-       <p v-for="item in user.skill" :key="item">{{item}}</p>
-     </li>
-   </ul> -->
-   <!-- <h2 v-for="(animal,index) in animals" :key="index">
-     <span v-if="animal !=='monkey'">{{animal}}::{{index}}</span>
-   </h2> -->
-   <template v-for="(animal,index) in animals" :key="index">
-    <h2 v-if="animal !=='monkey'">{{animal}}::{{index}} </h2>
-   </template>
+<!-- <h1>{{count}}</h1>
+ <button v-on:click="addNumber(1)">Increase</button>
+ <button v-on:click="minusNumber">Increase</button>
+ <button v-on:mouseover="addNumber(10)">Increase10 over mouse</button> -->
+ <h2>{{message}}</h2>
+ <!-- <button @click="greeting">Greet</button>
+  <button @click="goodbye">Bye</button> -->
+  <div @click="getMousePosition" class="box"></div>
+  <button @click="addNumber($event,10)">add10</button><button @click="addNumber($event,100)">add100</button>
 </template>
 
 <script>
 export default {
   name: 'App',
   data(){
-    return {
-      // animal:"Monkey",
-      // food:"banana",
-      // alertMessage:"<h2>ALERT</h2>",
-      // imageSource:"https://placeimg.com/100/100/any",
-      // naverUrl:"https://www.naver.com"
-      age:30,
-      display:true,
-      animals:["monkey","rat","dog","lion"],
-      users:[
-        {name:'scalper',job:'developer',gender:'male',skill:["html","css","javascript"]},
-        {name:'john',job:'designer',gender:'male',skill:["html","css","javascript"]},
-        {name:'mac',job:'developer',gender:'male',skill:["html","css","javascript"]}
-      ]
+    return{
+      count:0,
+      message:""
+    }
+  },
+  methods:{
+    getMousePosition(event){
+      console.log(event);
+      this.message=`Mouse Position:${event.pageX},${event.pageY}`;
+    },
+    greeting(){
+     this.message="Hello";
+    },
+    goodbye(){
+      this.message="Goodbye";
+    },
+    addNumber(e,value){
+      this.message=`mouse pos ${e.pageX} add ${value}=${e.pageX+value}`;
+    },
+    minusNumber(){
+      this.count=this.count-1;
     }
   }
   
@@ -62,23 +50,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
-input{
-  font-size: 20px;
+button{
+  width:100px;
+  height:100px;
+  font-size: 24px;
 }
-
-.orange{
-  color:orange;
-}
-.salmon{
-  color:salmon;
-}
-.red{
-  color:red;
-
-}
-
-.not-good{
-  text-decoration: line-through;
+.box{
+  width:300px;
+  height:300px;
+  background: salmon;
 }
 </style>
